467be4d913d5aa5829f1b99eb13ff6b8bce58c9b
Sung Paik <sung.paik@codeimmersives.com>
2019-10-10 14:31:12 -0400

    part one complete

--- main.js
+++ main.js
@@ -7,3 +7,18 @@ changeImg('#image-1', 'https://media.giphy.com/media/9xxciSRg2Ugwt76OQB/giphy.gi
 changeImg('#image-2', 'https://media.giphy.com/media/MFOW53R3kXP3hOEI71/giphy.gif');
 changeImg('#image-3', 'https://media.giphy.com/media/XbTygeYSO8bQ5FB02p/giphy.gif');
 
+function strikeText() {
+    const firstListItem = document.querySelector('ul :first-child');
+    firstListItem.style.textDecoration = 'line-through';
+}
+
+strikeText();
+
+function removeLi() {
+    const lastLi = document.querySelector('ul :last-child');
+    lastLi.remove();
+}
+
+removeLi();
+removeLi();
+
